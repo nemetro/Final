9cf7cb3924e0bf463292350fcd4d6fc6d8501bb0
scraplogic <noahh@umich.edu>
2015-03-11 14:27:03 -0400

    Added enemies and objective to project
    we have pathfinding enemies
    we have a keypad to open the laser door
    we have shooting enemies
    cool shit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float lifeTime = 1.0f;
+
+	void OnCollisionEnter(Collision other){
+		rigidbody.useGravity = true;
+
+		if (other.transform.tag == "Enemy") {
+			GameObject.Destroy(other.gameObject);
+		}
+	}
+
+	void Update(){
+		if (rigidbody.useGravity) {
+			lifeTime -= Time.deltaTime;
+		}
+
+		if (lifeTime < 0) {
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
