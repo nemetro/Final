311a81f6aa2df8942dc9548a9f9b240c00bfd91e
scraplogic <noahh@umich.edu>
2015-03-24 02:08:29 -0400

    added simple raycast gun
    recast gun can kill enemies and has sound

--- Assets/Attack Of The Robots Assets/Scripts/Player/WeaponMechanics.cs
+++ Assets/Attack Of The Robots Assets/Scripts/Player/WeaponMechanics.cs
@@ -6,14 +6,22 @@ public class WeaponMechanics : MonoBehaviour {
 
 	public GameObject gun;
 	public GameObject machete;
-//	public GameObject blueBullet;
 	public GameObject bulletHole;
+
 	public AudioClip gunShotSound;
-	public bool usingGun;
-	public float attackCooldown;
-	public bool justAttacked;
-	public InputDevice controller;
+	public AudioClip dryFireSound;
+	public AudioClip reloadSound;
 	
+	public InputDevice controller;
+	public int gunDamage = 60;
+	public float timeBetweenShots = 0.75f;
+	public float reloadTime = 2.0f;
+	public int maxBulletsInClip = 6;
+
+	private int bulletsInClip = 6;
+	private float attackCooldown;
+	private bool justAttacked;
+	private bool usingGun;
 
 	// Use this for initialization
 	void Start () {
@@ -57,16 +65,16 @@ public class WeaponMechanics : MonoBehaviour {
 		}
 
 		//Attacks
-
+		//TODO can probably make this modular later (class based with just a call to an attack() function.
 		//Melee weapon
 		if(usingGun == false) {
 			if (controller.LeftTrigger.WasPressed && attackCooldown <= 0) {
-				attackCooldown = .8f;
+				attackCooldown = 0.8f;
 				machete.transform.Rotate(0,90,0);
 				justAttacked = true;
 				machete.GetComponent<Collider>().enabled = true;
 			}
-			if(attackCooldown <= .5f && justAttacked) {
+			if(attackCooldown <= 0.5f && justAttacked) {
 				machete.transform.Rotate(0,-90,0);
 				justAttacked = false;
 				machete.GetComponent<Collider>().enabled = false;
@@ -75,17 +83,36 @@ public class WeaponMechanics : MonoBehaviour {
 
 		//gun
 		if(usingGun) {
-			GameObject bullet = null;
+			if(controller.LeftTrigger.WasPressed && attackCooldown <= 0 && bulletsInClip > 0){
+				attackCooldown = timeBetweenShots;
+				bulletsInClip--;
+				GetComponent<AudioSource>().PlayOneShot(gunShotSound);
 
-			if(controller.LeftTrigger.WasPressed){ 
-				Vector3 pos = gun.transform.position + gun.transform.forward;
-				pos = new Vector3(pos.x, pos.y, pos.z);
-				bullet = (GameObject)Instantiate(blueBullet, pos, transform.rotation);
-				bullet.GetComponent<Rigidbody>().AddForce(gun.transform.forward*40, ForceMode.VelocityChange);
-
+				FireGun();
+			} else if (controller.LeftTrigger.WasPressed && attackCooldown <= 0 && bulletsInClip <= 0){
+				GetComponent<AudioSource>().PlayOneShot(dryFireSound);
+			}
+			//TODO make this use controller
+			if(bulletsInClip < maxBulletsInClip && attackCooldown <= 0 && Input.GetKeyDown(KeyCode.R)){
+				attackCooldown = reloadTime;
+				GetComponent<AudioSource>().PlayOneShot(reloadSound);
+				bulletsInClip = maxBulletsInClip;
 			}
 		}
+	}
 
+	void FireGun(){
+		RaycastHit hitInfo;
+		if(Physics.Raycast(gun.transform.position, gun.transform.forward, out hitInfo)){ //if raycast hits something
+			GameObject bulletHoleClone = (GameObject) Instantiate(bulletHole, hitInfo.point + hitInfo.normal * 0.001f, Quaternion.FromToRotation(Vector3.up, hitInfo.normal)); //TODO Layer mask
+			if(hitInfo.transform.tag != "Wall"){
+				bulletHoleClone.GetComponent<Renderer>().enabled = false;
+			}
 
+			if (hitInfo.transform.tag == "Enemy"){
+				print ("Hit enemy");
+				hitInfo.transform.GetComponent<EnemyHealth>().ApplyDamage(gunDamage);
+			}
+		}
 	}
 }
