275befdb2290800fe104cc78a0989008b6b4523f
umichKevin <lank@umich.edu>
2015-03-31 17:45:43 -0400

    doors, and gnades
    added doors, gnades, and merged everytjing

---
+++ Assets/Attack Of The Robots Assets/Scripts/Player/OpenDoors.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoors : MonoBehaviour {
+	//public AnimationClip openDoor;
+
+	public bool doorClosed;
+	public Animation anim;
+
+	// Use this for initialization
+	void Start () {
+		doorClosed = true;
+		anim = GetComponentInParent<Animation> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	void OnTriggerStay(Collider other) {
+		if(doorClosed) {
+			if(Input.GetKeyDown ("e") && !GetComponentInParent<Animation>().IsPlaying("DoorOpenAnimation")) {
+				doorClosed = false;
+				GetComponentInParent<Animation>().Play("DoorOpenAnimation");
+				print("Open Door");
+
+			}
+		}
+		else if(!doorClosed) {
+			if(Input.GetKeyDown("e") && !GetComponentInParent<Animation>().IsPlaying("DoorOpenAnimation")) {
+				doorClosed = true;
+				GetComponentInParent<Animation>().Play("DoorCloseAnimation");
+			}
+		}
+	}
+}
