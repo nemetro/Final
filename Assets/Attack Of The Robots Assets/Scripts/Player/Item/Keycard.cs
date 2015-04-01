b0efc514346e0232e0495dcec1dc8c14e672e3b8
umichKevin <lank@umich.edu>
2015-04-01 00:05:28 -0400

    keycards and functionality
    keycards
    -you can pick them up!
    -they make sound when you pick them up
    
    lazer d00rz
    -you can now deacgtivate them and they won't send an alarm!

---
+++ Assets/Attack Of The Robots Assets/Scripts/Player/Item/Keycard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keycard : MonoBehaviour {
+	
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			//print ("ok");
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Attack Of The Robots Assets/Scripts/Player/PlayerInventory.cs
+++ Assets/Attack Of The Robots Assets/Scripts/Player/PlayerInventory.cs
@@ -4,4 +4,15 @@ using System.Collections;
 public class PlayerInventory : MonoBehaviour
 {
 	public bool hasKey;			// Whether or not the player has the key.
+	public AudioClip pickupSound;
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Keycard")
+		{
+			hasKey = true;
+			AudioSource.PlayClipAtPoint(pickupSound, transform.position, 1f);
+			print ("You got a keycard!");
+		}
+	}
 }
