8d47536880d460c3f4bd93413bd913501fded0f6
scraplogic <noahh@umich.edu>
2015-03-31 22:55:40 -0400

    working on making enemies track player better

--- Assets/Attack Of The Robots Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Attack Of The Robots Assets/Scripts/Enemy/EnemyAI.cs
@@ -5,16 +5,16 @@ public class EnemyAI : MonoBehaviour
 {
 	public float patrolSpeed = 2f;							// The nav mesh agent's speed when patrolling.
 	public float chaseSpeed = 5f;							// The nav mesh agent's speed when chasing.
-	public float chaseWaitTime = 5f;						// The amount of time to wait when the last sighting is reached.
-	public float patrolWaitTime = 1f;						// The amount of time to wait when the patrol way point is reached.
+	public float chaseWaitTime = 3f;						// The amount of time to wait when the last sighting is reached.
+	public float patrolWaitTime = 0f;						// The amount of time to wait when the patrol way point is reached.
 	public Transform[] patrolWayPoints;						// An array of transforms for the patrol route.
 	
 	
-	private EnemyDetectPlayer enemySight;						// Reference to the EnemySight script.
+	private EnemyDetectPlayer enemyDetectPlayer;						// Reference to the EnemySight script.
 	private NavMeshAgent nav;								// Reference to the nav mesh agent.
-	private Transform player;								// Reference to the player's transform.
-	private PlayerHealth playerHealth;					// Reference to the PlayerHealth script.
-	private GlobalLastPlayerSighting lastPlayerSighting;		// Reference to the last global sighting of the player.
+	private Transform targetPlayer;								// Reference to the player's transform.
+	private PlayerHealth targetPlayerHealth;					// Reference to the PlayerHealth script.
+//	private GlobalLastPlayerSighting lastPlayerSighting;		// Reference to the last global sighting of the player.
 	private float chaseTimer;								// A timer for the chaseWaitTime.
 	private float patrolTimer;								// A timer for the patrolWaitTime.
 	private int wayPointIndex;								// A counter for the way point array.
@@ -23,30 +23,28 @@ public class EnemyAI : MonoBehaviour
 	void Awake ()
 	{
 		// Setting up the references.
-		enemySight = GetComponent<EnemyDetectPlayer>();
+		enemyDetectPlayer = GetComponent<EnemyDetectPlayer>();
 		nav = GetComponent<NavMeshAgent>();
-		player = GameObject.FindGameObjectWithTag(InGameTags.player).transform;
-		playerHealth = player.GetComponent<PlayerHealth>();
-		lastPlayerSighting = GameObject.FindGameObjectWithTag(InGameTags.gameController).GetComponent<GlobalLastPlayerSighting>();
+		targetPlayer = GameObject.FindGameObjectWithTag(InGameTags.player).transform;
+		targetPlayerHealth = targetPlayer.GetComponent<PlayerHealth>();
+//		lastPlayerSighting = GameObject.FindGameObjectWithTag(InGameTags.gameController).GetComponent<GlobalLastPlayerSighting>();
 	}
 	
 	
 	void Update ()
 	{
 		// If the player is in sight and is alive...
-		if(enemySight.playerInSight && playerHealth.health > 0f)
+		if (enemyDetectPlayer.playerInSight && targetPlayerHealth.health > 0f) {
 			// ... shoot.
-			Shooting();
-		
-		// If the player has been sighted and isn't dead...
-		else if(enemySight.personalLastSighting != lastPlayerSighting.resetPosition && playerHealth.health > 0f)
+			Shooting ();
+//		} else if (enemySight.personalLastSighting != lastPlayerSighting.resetPosition && targetPlayerHealth.health > 0f) {// If the player has been sighted and isn't dead...
+		} else if (targetPlayerHealth.health > 0f) {// If the player has been sighted and isn't dead...
 			// ... chase.
-			Chasing();
-		
-		// Otherwise...
-		else
+			Chasing ();
+		} else {
 			// ... patrol.
-			Patrolling();
+			Patrolling ();
+		}
 	}
 	
 	
@@ -60,62 +58,53 @@ public class EnemyAI : MonoBehaviour
 	void Chasing ()
 	{
 		// Create a vector from the enemy to the last sighting of the player.
-		Vector3 sightingDeltaPos = enemySight.personalLastSighting - transform.position;
+		Vector3 sightingDeltaPos = enemyDetectPlayer.personalLastSighting - transform.position;
 		
 		// If the the last personal sighting of the player is not close...
 		if (sightingDeltaPos.sqrMagnitude > 4f) {
 			// ... set the destination for the NavMeshAgent to the last personal sighting of the player.
-			nav.destination = enemySight.personalLastSighting;
+			nav.destination = enemyDetectPlayer.personalLastSighting;
 		}
 		
-		// Set the appropriate speed for the NavMeshAgent.
-		nav.speed = chaseSpeed;
+
+		nav.speed = chaseSpeed;// Set the appropriate speed for the NavMeshAgent.
 		
-		// If near the last personal sighting...
-		if(nav.remainingDistance < nav.stoppingDistance)
-		{
-			// ... increment the timer.
-			chaseTimer += Time.deltaTime;
+
+		if (nav.remainingDistance < nav.stoppingDistance) { // If near the last personal sighting...
+
+			chaseTimer += Time.deltaTime;// ... increment the timer.
 			
-			// If the timer exceeds the wait time...
-			if(chaseTimer >= chaseWaitTime)
-			{
+
+			if (chaseTimer >= chaseWaitTime) { // If the timer exceeds the wait time...
 				// ... reset last global sighting, the last personal sighting and the timer.
-				lastPlayerSighting.position = lastPlayerSighting.resetPosition;
-				enemySight.personalLastSighting = lastPlayerSighting.resetPosition;
+//				lastPlayerSighting.position = lastPlayerSighting.resetPosition;
+//				enemyDetectPlayer.personalLastSighting = lastPlayerSighting.resetPosition;
 				chaseTimer = 0f;
 			}
-		}
-		else
+		} else {
 			// If not near the last sighting personal sighting of the player, reset the timer.
 			chaseTimer = 0f;
+		}
 	}
 
 	
-	void Patrolling ()
-	{
+	void Patrolling (){
 		// Set an appropriate speed for the NavMeshAgent.
 		nav.speed = patrolSpeed;
 		
-		// If near the next waypoint or there is no destination...
-		if (nav.destination == lastPlayerSighting.resetPosition || nav.remainingDistance < nav.stoppingDistance) {
-			// ... increment the timer.
-			patrolTimer += Time.deltaTime;
-			
-			// If the timer exceeds the wait time...
-			if (patrolTimer >= patrolWaitTime) {
-				// ... increment the wayPointIndex.
-				if (wayPointIndex == patrolWayPoints.Length - 1)
+		// If near the next waypoint...
+		if (nav.remainingDistance < nav.stoppingDistance) {
+			patrolTimer += Time.deltaTime; // ... increment the timer.
+			if (patrolTimer >= patrolWaitTime) { // If the timer exceeds the wait time...
+				wayPointIndex++;
+
+				if(wayPointIndex == patrolWayPoints.Length){ //wrap around waypoints when reach end
 					wayPointIndex = 0;
-				else
-					wayPointIndex++;
-				
-				// Reset the timer.
-				patrolTimer = 0;
+				}
+				patrolTimer = 0;// Reset the timer.
 			}
 		} else {
-			// If not near a destination, reset the timer.
-			patrolTimer = 0;
+			patrolTimer = 0;// If not near a destination, reset the timer.
 		}
 
 		// Set the destination to the patrolWayPoint.
