e12818aca28ecd03bf54f5972c937593895a968c
Troy <nemetro@umich.edu>
2015-04-01 00:10:53 -0400

    Working doors

---
+++ Assets/Attack Of The Robots Assets/Scripts/Level/OpenDoor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour {
+
+	public float dist;
+	public float speed;
+	public bool open = false;
+	public GameObject door;
+	
+	private float startpos;
+	private bool move = false;
+	
+	
+	void Start(){
+		startpos = door.transform.position.y;
+	}
+	
+	void FixedUpdate(){
+		if(move && !open){
+			door.transform.Translate(Vector3.up * speed);
+			if(door.transform.position.y - startpos >= dist){
+				move = false;
+				open = true;
+			}
+		}
+		else if(move && open){
+			door.transform.Translate(Vector3.down * speed);
+			if(door.transform.position.y - startpos <= 0){
+				move = false;
+				open = false;
+			}
+		}
+	}
+	
+	void OnTriggerStay (Collider other) 
+	{
+		print("Collide");
+		if(other.gameObject.tag == "Player"){
+			if(Input.GetKeyUp(KeyCode.E))
+			{
+				move = true;
+			}
+		}
+	}
+}
