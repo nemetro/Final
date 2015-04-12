d64d7f646ee3c87969eb9a292477c990379d50aa
Zack <zmherbs@umich.edu>
2015-04-11 22:04:46 -0400

    Pausing dully working

--- Assets/Attack Of The Robots Assets/Scripts/Player/PauseGame.cs
+++ Assets/Attack Of The Robots Assets/Scripts/Player/PauseGame.cs
@@ -4,20 +4,20 @@ using UnityEngine.UI;
 using InControl;
 
 public class PauseGame : MonoBehaviour {
-
+	
 	public Canvas normUI, pauseMenu;
-
-	GameObject[] players;
+	
+	public GameObject[] players;
 	GameObject sens;
 	InputDevice controller;
 	bool paused = false;
-
-	void Awake()
+	
+	void Start()
 	{
 		players = GameObject.FindGameObjectsWithTag("Player");
 		sens = GameObject.Find("SensitivityTracker");
 	}
-
+	
 	void Update () 
 	{
 		controller = InputManager.ActiveDevice;
@@ -27,7 +27,7 @@ public class PauseGame : MonoBehaviour {
 			Time.timeScale = 0;
 			normUI.gameObject.SetActive(false);
 			pauseMenu.gameObject.SetActive(true);
-			sens.SetActive(true);
+			//sens.SetActive(true);
 			for (int i = 0; i < players.Length; i++)
 			{
 				players[i].SetActive(false);
@@ -38,12 +38,13 @@ public class PauseGame : MonoBehaviour {
 		{
 			Time.timeScale = 1;
 			normUI.gameObject.SetActive(true);
+			sens.GetComponent<SensitivityTracker>().UpdateSensitivites();
 			pauseMenu.gameObject.SetActive(false);
-			sens.SetActive(false);
-			for (int i = 0; i < players.Length; i++)
+			//sens.SetActive(false);
+			/*for (int i = 0; i < players.Length; i++)
 			{
 				players[i].SetActive(true);
-			}
+			}*/
 			paused = false;
 		}
 	}
