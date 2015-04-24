d5435fa54b8262c43c1fc7fc4be481ee932d0dc0
scraplogic <noahh@umich.edu>
2015-04-24 12:25:57 -0400

    added score manager singleton class

--- Assets/Attack Of The Robots Assets/Scripts/GameManagement/ScoreManager.cs
+++ Assets/Attack Of The Robots Assets/Scripts/GameManagement/ScoreManager.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class ScoreManager : MonoBehaviour {
+	private static ScoreManager instance = null;
+
+	private static int currentScore = 0;
+	private static ArrayList scores;
+
+	public static ScoreManager Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		if (scores == null) {
+			scores = new ArrayList();
+		}
+		scores.Add (currentScore); //store score for later
+		currentScore = 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public static void AddPoints(int points){
+		currentScore += points;
 	}
 }
---
+++ Assets/Attack Of The Robots Assets/Scripts/Level/PointBall.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointBall : MonoBehaviour {
+	void OnTriggerEnter(Collider other){
+		if (other.tag == InGameTags.player) {
+			ScoreManager.AddPoints(1000);
+		}
+	}
+}
